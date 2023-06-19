c98d8a753c47fdc5b35ca248e0b4d9aa0872bc3b
Shaquille <shaqdulove@hotmail.com>
2023-06-19 00:49:33 -0400

    Updated Portfolio

--- src/components/main/Portfolio/index.tsx
+++ src/components/main/Portfolio/index.tsx
@@ -6,6 +6,37 @@ import "lightbox2/dist/css/lightbox.min.css";
 import "./styles.scss";
 // import 'lightbox2/dist/js/lightbox.js';
 
+interface PortfolioItem {
+  image: string;
+  narrow?: boolean;
+  categories: string[];
+  name: string;
+  link?: string;
+}
+
+const portfolioItems: PortfolioItem[] = [
+  {
+    name: "Merchive",
+    image: "/assets/images/portfolio2.png",
+    categories: ["React", "Figma", "Express", "MongoDB"],
+    link: "https://merchive.com",
+  },
+  {
+    name: "Ortex Login",
+    narrow: true,
+    image: "/assets/images/portfolio1.png",
+    categories: ["React"],
+    link: "https://ortex-login-shaquille.netlify.app",
+  },
+  {
+    name: "Indecision App",
+    narrow: true,
+    image: "/assets/images/portfolio3.png",
+    categories: ["React"],
+    link: "https://shaquille-indecision.netlify.app",
+  },
+];
+
 export default function Portfolio() {
   useEffect(() => {
     AOS.init({
@@ -35,145 +66,37 @@ export default function Portfolio() {
           </div>
 
           <div className="row portfolio-items">
-            <div className="col-md-12 scroll-animation" data-aos="fade-up">
-              <div className="portfolio-item portfolio-full">
-                <div className="portfolio-item-inner">
-                  <a
-                    href="../assets/images/portfolio1.jpg"
-                    data-lightbox="example-1"
-                  >
-                    <img
-                      src="../assets/images/portfolio1.jpg"
-                      alt="Portfolio"
-                    />
-                  </a>
-                  <ul className="portfolio-categories">
-                    <li>
-                      <a href="https://www.figma.com"> Figma </a>
-                    </li>
-                    <li>
-                      <a href="https://www.Framer.com"> Framer </a>
-                    </li>
-                    <li>
-                      <a href="https://www.wordpress.com"> WordPress </a>
-                    </li>
-                  </ul>
+            {portfolioItems.map((item, i) => (
+              <div
+                key={item.image}
+                className={`col-md-${
+                  item.narrow ? "6" : "12"
+                } scroll-animation`}
+                data-aos={`fade-${
+                  i % 3 === 0 ? "right" : i % 3 === 1 ? "up" : "left"
+                }`}
+              >
+                <div className="portfolio-item portfolio-full">
+                  <div className="portfolio-item-inner">
+                    <a href={item.image} data-lightbox="example-1">
+                      <img src={item.image} alt="Portfolio" />
+                    </a>
+                    <ul className="portfolio-categories">
+                      {item.categories.map((category) => (
+                        <li key={category}>
+                          <a> {category} </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                  <h2>
+                    <a href={item.link} target="_blank">
+                      {item.name}
+                    </a>
+                  </h2>
                 </div>
-                <h2>
-                  <a href="https://www.Bureau - Architecture Studio Website.com">
-                    Bureau - Architecture Studio Website
-                  </a>
-                </h2>
               </div>
-            </div>
-            <div className="col-md-6 scroll-animation" data-aos="fade-right">
-              <div className="portfolio-item portfolio-half">
-                <div className="portfolio-item-inner">
-                  <a
-                    href="../assets/images/portfolio2.jpg"
-                    data-lightbox="example-1"
-                  >
-                    <img
-                      src="../assets/images/portfolio2.jpg"
-                      alt="Portfolio"
-                    />
-                  </a>
-                  <img src="../assets/images/portfolio2.jpg" alt="Portfolio" />
-                  <ul className="portfolio-categories">
-                    <li>
-                      <a href="https://www.wordpress.com"> WordPress </a>
-                    </li>
-                    <li>
-                      <a href="https://www.larevel.com"> Larevel / PHP </a>
-                    </li>
-                  </ul>
-                </div>
-                <h2>
-                  <a href="https://www.moonex.com"> Moonex WordPress Theme </a>
-                </h2>
-              </div>
-            </div>
-            <div className="col-md-6 scroll-animation" data-aos="fade-left">
-              <div className="portfolio-item portfolio-half">
-                <div className="portfolio-item-inner">
-                  <a
-                    href="../assets/images/portfolio3.jpg"
-                    data-lightbox="example-1"
-                  >
-                    <img
-                      src="../assets/images/portfolio3.jpg"
-                      alt="Portfolio"
-                    />
-                  </a>
-                  <ul className="portfolio-categories">
-                    <li>
-                      <a href="https://www.figma.com"> Figma </a>
-                    </li>
-                    <li>
-                      <a href="https://www.webflow.com"> Webflow </a>
-                    </li>
-                  </ul>
-                </div>
-                <h2>
-                  <a href="https://www.taskly-dashboard.com">
-                    Taskly Dashboard
-                  </a>
-                </h2>
-              </div>
-            </div>
-            <div className="col-md-12 scroll-animation" data-aos="fade-up">
-              <div className="portfolio-item portfolio-half">
-                <div className="portfolio-item-inner">
-                  <a
-                    href="../assets/images/portfolio4.jpg"
-                    data-lightbox="example-1"
-                  >
-                    <img
-                      src="../assets/images/portfolio4.jpg"
-                      alt="portfolio"
-                    />
-                  </a>
-                  <ul className="portfolio-categories">
-                    <li>
-                      <a href="https://www.figma.com"> Figma </a>
-                    </li>
-                    <li>
-                      <a href="https://www.reactjs.com"> React </a>
-                    </li>
-                  </ul>
-                </div>
-                <h2>
-                  <a href="https://www.hinterland.com">
-                    Hinterland - Real Estate Site Redesign
-                  </a>
-                </h2>
-              </div>
-            </div>
-            <div className="col-md-12 scroll-animation" data-aos="fade-up">
-              <div className="portfolio-item portfolio-half">
-                <div className="portfolio-item-inner">
-                  <a
-                    href="../assets/images/portfolio5.jpg"
-                    data-lightbox="example-1"
-                  >
-                    <img
-                      src="../assets/images/portfolio5.jpg"
-                      alt="portfolio"
-                    />
-                  </a>
-                  <ul className="portfolio-categories">
-                    <li>
-                      <a href="https://www.framer.com"> Framer </a>
-                    </li>
-                  </ul>
-                </div>
-                <h2>
-                  <a href="https://www.lewis.com">
-                    Lewis Portfolio Framer Template
-                  </a>
-                </h2>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
